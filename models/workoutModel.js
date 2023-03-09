a9f2e95c4fc62d4c04c8f04d2fb155f9ac8090c4
he4a00 <he4a00@github.com>
2023-03-10 00:09:18 +0200

    initial commit

---
+++ models/workoutModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    user_id: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Workout", workoutSchema);
